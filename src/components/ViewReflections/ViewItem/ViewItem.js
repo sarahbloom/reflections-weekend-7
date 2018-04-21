1970796d37fa5eac24635981f78c6cecf2fed5d8
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-04-20 20:36:11 -0500

    formatted date

--- src/components/ViewReflections/ViewItem/ViewItem.js
+++ src/components/ViewReflections/ViewItem/ViewItem.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
+import moment from 'moment';
+import Moment from 'react-moment';
 
 class ViewItem extends Component{
 
     render(){
+        let date = this.props.listItem.date  
+
         return <div> <h3>{this.props.listItem.topic} </h3>
         <h4>{this.props.listItem.description} </h4>
-        <p> Date of event: {this.props.listItem.date} </p></div>
+            <p> Date of event: <Moment format= 'MMMM Do, YYYY'>{date}</Moment> </p></div>
 
         //TODO: format date
-
     }
 }
 
